77b31afb61ae1a834e1f365ea7b10fe4189f9ca2
manuelpc97 <manuelpc97@unitec.edu>
2016-02-26 14:02:42 -0600

    Primera pantalla desplegada

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <ncurses.h>
 
 int main(int argc, char*argv[]){
-	
+	int x, y;
+
+	initscr();
+	getmaxyx(stdscr, x, y);
+	move(x/2,( y/2)-16);
+	printw("Bienvenido a tu agenda de amigos");
+	move((x/2)+1,( y/2)-16);
+	printw("Laboratorio 5 de Programacion3");
+	getch();
+	endwin();
+			
 	return 0;
 }
