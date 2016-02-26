ee1926dd5a6f2b44484bde0f526391fe625d834b
lesterarte <lesterarte007@unitec.edu>
2016-02-26 14:46:38 -0600

    Almacena el nombre

--- main.cpp
+++ main.cpp
@@ -3,14 +3,10 @@
 
 
 int main(int argc, char*argv[]){
-<<<<<<< HEAD
-
-
 
 	int x, y;
-	int salir; 
-
-	int x, y;
+	char nombre[25]; 
+	char numero[9];
 	char opcion;
 	initscr();
 	getmaxyx(stdscr, x, y);
@@ -18,8 +14,8 @@ int main(int argc, char*argv[]){
 	printw("Bienvenido a tu agenda de amigos");
 	move((x/2)+1,( y/2)-16);
 	printw("Laboratorio 5 de Programacion3");
-	getch();
-	endwin();
+	
+	
 
 	
 	if(getch()){
@@ -41,6 +37,14 @@ int main(int argc, char*argv[]){
 			clear();
 			refresh();
 			endwin();
+		}
+		if(opcion=='1'){
+			clear();
+			move((x/2)-4,( y/2)-16);
+			printw("Nombre del Amigo: ");
+			getstr(nombre);
+			move((x/2)-3,( y/2)-16);
+			printw("Numero del amigo: ");
 		}
 	}
 
